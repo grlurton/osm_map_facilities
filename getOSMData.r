0401ee1d7f765e3200439b30d7ab32ea8e0dced0
Gregoire Lurton <grlurton@ihme.washington.edu>
2016-03-18 13:12:05 -0700

    updated osm extraction

--- getOSMData.r
+++ getOSMData.r
@@ -16,56 +16,44 @@ response <- getURL(url)
 resp <- xmlParse(response)
 osmardata <- as_osmar(resp)
 shapefile <- as_sp(osmardata, what="points")
-
+par(mfrow = c(2,2))
+plot(shapefile)
 
 ##Crop to the size of Nigeria
 
 NigeriaShp@proj4string <- shapefile@proj4string
 
 osm_data <- shapefile[!is.na(over(shapefile , NigeriaShp)$UnitName) ,]
-
-plot(shapefile)
-plot(osm_data , add = T , col = 'red')
-
-
-
-
-
-
+plot(osm_data)
 
 ## Reshaping a few names
 tt <- osmardata$nodes$tags
 tt$k <- as.character(tt$k)
+tt$v <- as.character(tt$v)
+
+library(plyr)
+n_values <- ddply(tt , .(k) , nrow)
+
+
+## taking out indicators 
+tt <- tt[!(tt$k %in% c("GNS:id" , "gns:dsg" , "GNS:dsg_code" , "is_in:country" , "GNS:dsg_string" , "fixme" , 
+                       'wikipedia' ,
+                       n_values$k[n_values$V1 < 7])) & 
+                        substr(tt$k , 1, 5 ) != 'name:' & substr(tt$k , 1, 14) != 'official_name:' &
+           !(tt$v %in% c('Cameroon','Chad','Benin')), ]
 tt$k <- gsub(':' , '_'  ,tt$k)
 tt$k <- gsub('/' , '_'  ,tt$k)
 tt$k <- gsub(' ' , '_'  ,tt$k)
 tt$k <- gsub('__' , '_'  ,tt$k)
-length(unique(tt$k))
 
-reshTags <- dcast(tt , id ~ k)
+reshTags <- dcast(tt , id ~ k ,  value.var = 'v')
 
-colnames(reshTags)[1] <- colnames(shapefile@data)[1] <- 'idtoMatch'
+colnames(reshTags)[1] <- colnames(osm_data@data)[1] <- 'idtoMatch'
 
-shapefile@data <- merge(shapefile@data , reshTags , by = 'idtoMatch' ,
-                        sort = FALSE)
-
+osm_data@data <- merge(osm_data@data , reshTags , by = 'idtoMatch' ,
+                        sort = FALSE , all.y = FALSE )
 
 
-
-
-
-
-
-
-
-
-
-
-plot(NigeriaShp)
-plot(shapefile , add = TRUE)
-
-plot(NigeriaShp)
-plot(shapefile , col = factor(shapefile@data$amenity) , add = TRUE)
 
 t <- function(data){
   for(i in 1:ncol(data)){
@@ -77,11 +65,9 @@ t <- function(data){
   data
 }
 
-shapefile@data <- t(shapefile@data)
+osm_data@data <- t(osm_data@data)
 
-writePointsShape(osm_data, "OSMDataNigeria")
+writePointsShape(osm_data, "data/OSMDataNigeria")
 
 
-
-rm(list = ls()[ls() != c('NigeriaShp' , 'osm_data')])
-
+rm(list = ls()[!(ls() %in% c('NigeriaShp' , 'osm_data'))])
